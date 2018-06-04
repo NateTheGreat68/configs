5958a1836d8e0255c882fa2a21f339056d5a2b47
Nathan Freeman <nfreeman68@gmail.com>
2018-06-03 22:12:55 -0400

    Added an installer for bashrc configs.

---
+++ installers/bashrc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+
+echo "Installing bashrc configs..."
+
+DIR="$(dirname "$(readlink -f "$0")")"
+
+# make the directory if it doesn't exist
+[ -d "$HOME/.configs" ] || mkdir "$HOME/.configs"
+
+# copy the files over
+cp -v "$DIR"/../bash_* "$HOME/.configs/"
+
+# add bashrc lines if they aren't already there
+grep -Fxq "$(tail -n 1 "$DIR/bashrc")" "$HOME/.bashrc" || cat "$DIR/bashrc" >> "$HOME/.bashrc"
